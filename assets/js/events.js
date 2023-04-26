0854c47bf1fa9106d9004b572b0adee01bcdff80
Luis Cu <luiscu@galileo.edu>
2023-04-26 11:45:33 -0600

    base website

---
+++ assets/js/events.js
@@ -0,0 +1,25 @@
+var events = [
+    {
+        "name": "Ejemplo",
+        "description": "Descripción del evento ejemplo",
+        "speaker": "Elisa Ejemplo",
+        "day": "19",
+        "time": "09:00",
+        "place": "Track 1 - Salón 401",
+        "subjects": [],
+        "tba": "0"
+    }
+    
+/*
+    {
+        "name": "",
+        "description": "",
+        "speaker": "",
+        "day": "25",
+        "time": "09:00",
+        "place": "Track 1 - Salón 401",
+        "subjects": [],
+        "tba": "0"
+    },
+*/
+];
---
+++ assets/js/staff.js
@@ -0,0 +1,12 @@
+var staff = [
+
+    /* COLOR VERDE EN LISTADO */ 
+
+    {
+        "name": "Elisa Ejemplo",
+        "short": "",
+        "information": "Información informativa.",
+        "image": "default.png",
+        "nationality": "guatemala"
+    }
+];
